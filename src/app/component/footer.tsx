e247bd74aa07dd97f630621ecb2d7df9d17603e2
DenisFirmansyah <denisfirmansyah55@gmail.com>
2025-06-13 23:36:57 +0700

    minor update

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -4,24 +4,22 @@ import Link from "next/link"
 
 export const Footer = (): React.ReactElement => {
     return (
-        <div className="grid grid-cols-3 px-5 py-10 bg-gray-400 align-middle">
+        <div className="grid grid-cols-2 px-5 py-10 bg-gray-200 align-middle text-black">
             <div>
-                <h2 className="text-2xl">SunSaKi</h2>
+                <strong className="text-3xl">SunSaKi</strong>
                 <p className="text-lg">Game Developer and Front End Developer</p>
+                <p>&copy; 2025, SunSaKi. All rights reserved.</p>
             </div>
             <div className="flex justify-center gap-4">
                 <Link href="https://instagram.com/_denizzf">
-                    <FontAwesomeIcon icon={faInstagram} className="w-10 h-10 hover:text-blue-900 transition-colors" />
+                    <FontAwesomeIcon icon={faInstagram} fixedWidth className="w-10 h-10 hover:text-blue-900" />
                 </Link>
                 <Link href="https://linkedin.com/in/denis-firmansyah">
-                    <FontAwesomeIcon icon={faLinkedin} className="w-10 h-10 hover:text-blue-900 transition-colors" />
+                    <FontAwesomeIcon icon={faLinkedin} fixedWidth className="w-10 h-10 hover:text-blue-900" />
                 </Link>
                 <Link href="https://github.com/DenisFirmansyah">
-                    <FontAwesomeIcon icon={faGithub} className="w-10 h-10 hover:text-blue-900 transition-colors" />
+                    <FontAwesomeIcon icon={faGithub} fixedWidth className="w-10 h-10 hover:text-blue-900" />
                 </Link>
-            </div>
-            <div className="flex flex-grow items-center">
-                <p>&copy; 2025, SunSaKi. All rights reserved.</p>
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,37 @@
+'use client'
+
 import Image from "next/image";
 import { Footer } from "./component/footer";
 import { NavBar } from "./component/navbar";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Project = {
+  id: number,
+  name: string,
+  body: string,
+  type: string
+}
 
 export default function Home() {
+  const [data, setData] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch ('/data.json');
+      const data = await res.json();
+      setData(data);
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div className="bg-blue-900">
+    <div className="bg-blue-950">
       <NavBar />
-      <main className="px-5 py-12 text-2xl text-gray-300">
-        <section className="flex">
-          <div className="px-10">
+      <main className="px-5 py-12 border-t-1 sm:text-xl md:text-2xl text-gray-300">
+        <section className="sm:flex">
+          <div className="px-10 mb-4 sm::mb-0">
             <Image src="/images/profile.jpg" alt="" width={300} height={100}></Image>
           </div>
           <div className="">
@@ -21,17 +43,17 @@ export default function Home() {
         <div className="w-full max-w-2xl mx-auto pt-12 mb-12 border-b-1 border-b-white"></div>
         <section>
           <h1 className="text-center text-5xl mb-5">Know About Me</h1>
-          <div className="flex">
+          <div className="sm:flex">
             <Image src="/images/photo-transparent.png" alt="" width={400} height={100} className="px-10"/>
             <div className="max-w-4xl">
               <p>I am an individual who is oriented and has a strong interest in technology and creative fields. My good skills can produce something satisfying for those who enjoy my work, and also myself.</p>
               <br />
               <ul>
-                <li><strong>Full Name:</strong> Denis Firmansyah</li>
-                <li><strong>Age:</strong> 20</li>
-                <li><strong>City:</strong> West Bandung, West Java, Indonesia</li>
-                <li><strong>Last Education:</strong> UIN SGD Bandung</li>
-                <li><strong>Profession:</strong> Traditional 2D Illustrator, Digital 2D Illustrator, 3D Designer, Narrative Designer, Game Programmer, Front-End Developer</li>
+                <li><strong className="text-white">Full Name:</strong> Denis Firmansyah</li>
+                <li><strong className="text-white">Age:</strong> 20</li>
+                <li><strong className="text-white">City:</strong> West Bandung, West Java, Indonesia</li>
+                <li><strong className="text-white">Last Education:</strong> UIN SGD Bandung</li>
+                <li><strong className="text-white">Profession:</strong> Traditional 2D Illustrator, Digital 2D Illustrator, 3D Designer, Narrative Designer, Game Programmer, Front-End Developer</li>
               </ul>
               <br />
               <p>See my Resume <Link href="https://drive.google.com/file/d/13JWxZufi6pA3OqZ2HGj9Kb7mNdYbQVfR/view?usp=sharing" className="underline text-white hover:text-gray-300">here</Link></p>
@@ -39,9 +61,9 @@ export default function Home() {
           </div>
         </section>
         <div className="w-full max-w-2xl mx-auto pt-12 mb-12 border-b-1 border-b-white"></div>
-        <section className="text-xl">
-          <h1 className="text-center text-5xl mb-5">Behold My Power</h1>
-          <div className="grid grid-cols-3 gap-5 w-full mx-auto">
+        <section className="">
+          <h1 className="text-center text-3xl sm:text-5xl mb-5">Behold My Power</h1>
+          <div className="grid sm:grid-cols-3 gap-5 w-full mx-auto">
             <div className="border-1 rounded-sm p-4">
               <h2>JavaScript</h2>
               <p>Mampu membuat fungsi yang diterpakan untuk Front-end website</p>
@@ -88,9 +110,17 @@ export default function Home() {
           </div>
         </section>
         <div className="w-full max-w-2xl mx-auto pt-12 mb-12 border-b-1 border-b-white"></div>
-        <section className="text-xl">
-          <h1 className="text-center text-5xl mb-5">Let&apos;s See My Project Galery</h1>
-          
+        <section>
+          <h1 className="text-center text-3xl sm:text-5xl mb-5">Let&apos;s See My Project Galery</h1>
+
+          <div className="grid sm:grid-cols-3 gap-5">
+            {data.map((item) => (
+              <div key={item.id} className="border-1 p-4 rounded-sm">
+                <h2>{item.name}</h2>
+                <p>{item.body}</p>
+              </div>
+            ))}
+          </div>
         </section>
       </main>
       <Footer />
